b51bbeeac10f3c75e26154155e6d87c6c9b0cd5a
Caber Chu <caberchu@clearcubic.com>
2014-12-11 22:57:54 -0500

    Add Season to core models.

--- core/models.py
+++ core/models.py
@@ -1,5 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# from datetime import date
+
 from django.db import models
 
 class Base(models.Model):
 	class Meta:
 		abstract = True
+
+class Season():
+	SPRING = 's'
+	FALL = 'f'
+	SEASON_CHOICES = (
+		(SPRING, u'Spring (春)'),
+		(FALL, u'Fall (秋)'),
+	)
+
+	@staticmethod
+	def get_season_text(season):
+		SEASON_TEXT = {
+			Season.SPRING: u'Spring (春)',
+			Season.FALL: u'Fall (秋)',
+		}
+		return SEASON_TEXT[season]
